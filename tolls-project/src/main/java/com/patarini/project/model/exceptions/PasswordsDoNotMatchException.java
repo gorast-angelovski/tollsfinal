98dd61fa456adb201835b9b27497c42f4a5b9b8f
gorast-angelovski <74464158+gorast-angelovski@users.noreply.github.com>
2021-02-17 13:20:54 +0100

    Delete tolls-project directory

--- tolls-project/src/main/java/com/patarini/project/model/exceptions/PasswordsDoNotMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.patarini.project.model.exceptions;
-
-public class PasswordsDoNotMatchException extends RuntimeException{
-
-    public PasswordsDoNotMatchException() {
-        super("Passwords do not match exception.");
-    }
-}
